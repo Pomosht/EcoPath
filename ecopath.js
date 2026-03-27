e054cda0df12fd87943c3ea515d6dab2356330ff
Pomosht <konstantin.petkov.2011@gmail.com>
2026-03-27 09:08:14 +0200

    fixing small js things

--- ecopath.js
+++ ecopath.js
@@ -60,7 +60,7 @@ async function calculateRoute() {
         document.getElementById('timeVal').innerText = `${Math.floor(summary.duration/3600)}ч ${Math.floor((summary.duration%3600)/60)}м`;
 
         // КОРЕКЦИЯ НА ЕКО РЕЙТИНГА: По-реалистична скала
-        let factor = transport === 'electric' ? 0.05 : transport === 'bus' ? 0.03 : transport === 'bike' ? 0 : 0.14;
+        let factor = transport === 'electric' ? 0.05 : transport === 'bus' ? 0.08 : transport === 'bike' ? 0 : 0.14;
         const co2 = distKm * factor;
         document.getElementById('co2Val').innerText = co2.toFixed(1);
 
